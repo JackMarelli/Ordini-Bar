e1ca9cf909d7b5c4df41cc61ac39429d807c7e16
JackMarelli <marelligiacomo@gmail.com>
2021-10-07 09:09:48 +0200

    aggiunto visuallizza psw

--- Website/js/animazioni.js
+++ Website/js/animazioni.js
@@ -1,3 +1,11 @@
-function fadeOverlay() {
-    document.getElementById("overlay").style.visibility = "hidden";
+function vedipsw() {
+    if (document.getElementById("pswinput").type = "password") {
+        document.getElementById("pswinput").type = "text";
+    }
+    else if (document.getElementById("pswinput").type = "text") {
+        document.getElementById("pswinput").type = "password";
+    }
 }
+
+//psw mail:
+//Redmenu2021
