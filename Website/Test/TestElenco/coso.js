9d0282278dd80e7992f0b1af7aa46a1ec456c33c
Luca-Lucidera <82151241+Luca-Lucidera@users.noreply.github.com>
2021-10-14 13:32:35 +0200

    Aggiunta la creazione automatica degli elementi del dom da un file JSON con funzioni di incremento e decremento (CARTELLA DI PROVA)

--- Website/Test/TestElenco/coso.js
+++ Website/Test/TestElenco/coso.js
@@ -1,34 +1,68 @@
-var json='[{"nome":"panino","prezzo":"1"},{"nome":"pizza","prezzo":"2"},{"nome":"kebab","prezzo":"50"}]';
+var json = '[{"nome":"panino","prezzo":"1"},{"nome":"pizza","prezzo":"2"},{"nome":"kebab","prezzo":"50"}]';
 const obj = JSON.parse(json);
-var Quantita = [];
+var Quantita = []; //vettore per le quantità di ogni singolo elemento
 var x = 0;
 var lunghezza = 0;
 obj.forEach(element => {
     lunghezza++;
 });
-
 for (let index = 0; index < lunghezza; index++) {
-    Quantita[index] = x; 
+    Quantita[index] = x;
 }
-function incrementa(nome){
-    var span = document.getElementById(nome); // find the <span> element in the DOM
-    for(var i=0;i<lunghezza;i++){
-        if(obj[i].nome == nome){
-           Quantita[i] = Quantita[i]+1; 
-           span.textContent = Quantita[i];
-           break;
+function incrementa(nome) { //il paramentro non è l'id ma è l'oggetto HTML
+    for (var i = 0; i < lunghezza; i++) {
+        if (obj[i].nome == nome.id) {
+            Quantita[i] = Quantita[i] + 1;
+            nome.textContent = Quantita[i];
+            break;
         }
-    }   
+    }
 }
-function decrementa(nome){
-    var span = document.getElementById(nome); // find the <span> element in the DOM
-    for(var i=0;i<lunghezza;i++){
-        if(obj[i].nome == nome){
-            if(Quantita[i] != 0){
-                Quantita[i] = Quantita[i]-1; 
-                span.textContent = Quantita[i];
+function decrementa(nome) { //il paramentro non è l'id ma è l'oggetto HTML
+    for (var i = 0; i < lunghezza; i++) {
+        if (obj[i].nome == nome.id) {
+            if(Quantita[i]>0){
+                Quantita[i] = Quantita[i] - 1;
+                nome.textContent = Quantita[i];
             }
-           break;
+            break;
         }
-    }  
+    }
+}
+
+function CaricaPagina() {
+    for (let index = 0; index < lunghezza; index++) {
+        
+        var nomeAttributo = obj[index].nome;
+        //creazione degli elementi
+        const paragrafo = document.createElement("p");
+        const span = document.createElement("span");
+        const bottoneAumenta = document.createElement("button");
+        const bottoneDecrementa = document.createElement("button");
+        const spazio = document.createElement("br");
+
+        paragrafo.innerHTML = nomeAttributo;
+        
+        //aggiunti attributi per lo span dov'è contenuto il numero
+        span.setAttribute('id', nomeAttributo);
+        span.innerHTML = 0;
+
+        //aggiunti attributi ai bottoni per incremento
+        var funzione = "incrementa(" + nomeAttributo+")";
+        bottoneAumenta.setAttribute('onclick',funzione);
+        bottoneAumenta.innerHTML = "+";
+
+        //aggiunti attributi ai bottoni per decremento
+        funzione = "decrementa(" + nomeAttributo+")";
+        bottoneDecrementa.setAttribute('onclick',funzione);
+        bottoneDecrementa.innerHTML = "-";
+
+        //ora vado a inserire gli elementi nel body
+        var body = document.querySelector('body');
+        body.appendChild(paragrafo);
+        body.appendChild(span);
+        body.appendChild(bottoneAumenta);
+        body.appendChild(bottoneDecrementa);
+        body.append(spazio);
+    }
 }
