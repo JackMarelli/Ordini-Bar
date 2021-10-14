bd539e8c1527d1fbe15c1d95df2f4b30116ea275
DanieleRonco <82151369+DanieleRonco@users.noreply.github.com>
2021-10-14 08:41:41 +0200

    Logo nero e bug
    Aggiunta la cartella contenente il logo con scritta nera e logo rosso.
    Correzione bug password.

--- Website/js/animazioni.js
+++ Website/js/animazioni.js
@@ -1,8 +1,8 @@
 function vedipsw() {
-    if (document.getElementById("pswinput").type = "password") {
+    if (document.getElementById("pswinput").type == "password") {
         document.getElementById("pswinput").type = "text";
     }
-    else if (document.getElementById("pswinput").type = "text") {
+    else if (document.getElementById("pswinput").type == "text") {
         document.getElementById("pswinput").type = "password";
     }
 }
