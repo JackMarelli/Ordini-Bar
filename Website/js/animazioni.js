1fbf56b83e6ea1732a36f1af84bfdfc3e804924e
JackMarelli <marelligiacomo@gmail.com>
2021-10-20 11:15:43 +0200

    mainlist.html finita

--- Website/js/animazioni.js
+++ Website/js/animazioni.js
@@ -9,10 +9,12 @@ function vedipsw() {
 
 function overlayTranslateIn() {
     document.getElementById("overlaymenu").style.transform = "translate(0, 0)";
+    document.getElementById("menubutton").style.visibility = hidden;
 }
 
 function overlayTranslateOut() {
     document.getElementById("overlaymenu").style.transform = "translate(0, -100%)";
+    document.getElementById("menubutton").style.visibility = visible;
 }
 
 //psw mail:
