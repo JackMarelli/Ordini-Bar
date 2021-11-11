1dfdd9edb93909c5e459b0dd27762fce1660afb5
Luca-Lucidera <lucidera.luca@gmail.com>
2021-11-11 16:25:14 +0100

    cronologia.html e .js completati Ora la pagina viene caricata correttamente e reindirizza correttamente alla pagina degli ordini

--- Website/js/cronologia.js
+++ Website/js/cronologia.js
@@ -1,25 +1,45 @@
-var dati;
-const ordine = "ordine1";
-let promise = new Promise(function(Resolve){
-    fetch("../js/tmp.json").then(response => Resolve( response.json()));
+let promise = new Promise(function (Resolve) {
+  fetch("../js/tmp.json").then((response) => Resolve(response.json()));
 });
-promise.then(
-    function(value){
-        let json = value;
-        caricaPagina(json);
+promise.then(function (value) {
+  let json = value;
+  caricaPagina(json);
 });
 
-function caricaPagina(json){
-    /*
-    for (let i = 0; i < json.length; i++) {
-        for (let j = 0; j < json[i].ordine.length; j++) {
-            console.log(json[i].ordine[j]);
-        }
+function caricaPagina(json) {
+  for (let i = 0; i < json.length; i++) {
+    const divOrdine = document.createElement("div");
+    const divNome = document.createElement("div");
+    const divTotale = document.createElement("div");
+    divOrdine.setAttribute("class", "ordine");
+    divNome.setAttribute("class", "nome");
+    divNome.setAttribute("id", i);
+    divNome.setAttribute("onclick", "caricaOrdine(id)");
+    divTotale.setAttribute("class", "totale");
+    divNome.textContent = "Ordine " + (i + 1);
+    let totale = 0;
+    for (let j = 0; j < json[i].ordine.length; j++) {
+      totale +=
+        parseFloat(json[i].ordine[j].prezzo.replace(",", ".")) *
+        json[i].ordine[j].quantita;
     }
-    ciclo per prendere i singoli elementi.
-    */ 
-    const divOrdine = document.querySelector("div");
-   const divNome = document.querySelector("div");
-   const divTotale = document.querySelector("div");
- 
+    divTotale.textContent = totale;
+    const divLista = document.querySelector(".lista");
+    divLista.appendChild(divOrdine);
+    divOrdine.appendChild(divNome);
+    divOrdine.appendChild(divTotale);
+  }
+}
+function caricaOrdine(id) {
+  //id = numero dell'ordine in base al json
+  let promise = new Promise(function (Resolve) {
+    fetch("../js/tmp.json").then((response) => Resolve(response.json()));
+  });
+  promise.then(function (value) {
+    let json = value;
+    localStorage.clear();
+    let daSalvare = JSON.stringify(json[id].ordine);
+    localStorage.setItem("json", daSalvare);
+    window.location.replace("../pages/ordine.html");
+  });
 }
