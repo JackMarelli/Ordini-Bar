4f038f620555320c54076dee5b2cdf9a5ca49a46
Luca-Lucidera <82151241+Luca-Lucidera@users.noreply.github.com>
2021-11-22 11:57:16 +0100

    Completato codice javascript per scansione del qr e passaggio dei dati alla pagina ordine.html

--- Website/js/admin/listaordini/qrscanner.js
+++ Website/js/admin/listaordini/qrscanner.js
@@ -1,5 +1,8 @@
 function onScanSuccess(decodedText, decodedResult) {
     console.log(`Code scanned = ${decodedText}`, decodedResult);
+    //TODO: salva il codice in localstorage con nome primaryKey
+    localStorage.setItem("primaryKey",decodedText);
+    window.location.href = "./../../../pages/admin/listaordini/ordine.html"
 }
 
 function CaricaQR(){
