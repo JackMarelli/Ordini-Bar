ebd167bb1c824a787ad6013b99e7ee7ae6817628
DanieleRonco <82151369+DanieleRonco@users.noreply.github.com>
2021-11-25 13:15:26 +0100

    Ketchup e Maionese
    Visualizzazione della quantità di ketchup e maionese nella pagina Ordine (Admin).

--- Website/js/admin/listaordini/ordine.js
+++ Website/js/admin/listaordini/ordine.js
@@ -7,12 +7,19 @@ function CaricaPagina(){
     document.getElementById("orario").innerHTML = localStorage.getItem("orario");
 
     //SI EFFETTUA LA RICHIESTA AL SERVER PER OTTENERE TUTTI I PRODOTTI DELL'ORDINE AVENTE LA PRIMARYBEY INDICATA
-    var json = '{[{"nome":"Panino","prezzo":"2","quantita":"1"},{"nome":"Pizza con patatine","prezzo":"2,50","quantita":"2"},{"nome":"Panzerotto","prezzo":"1,50","quantita":"3"},{"nome":"Focaccia","prezzo":"1","quantita":"1"}]}';
+    var json = '{"prodotti":[{"nome":"Panino","prezzo":"2","quantita":"1"},{"nome":"Pizza con patatine","prezzo":"2,50","quantita":"2"},{"nome":"Panzerotto","prezzo":"1,50","quantita":"3"},{"nome":"Focaccia","prezzo":"1","quantita":"1"}], "ketchup" : "2", "maionesi" : "15"}';
 
     //Si fa il parse del JSON
+    
     const obj = JSON.parse(json);
+    const prodotti = obj.prodotti;
+    var numeroKetchup = obj.ketchup;
+    var numeroMaionese = obj.maionesi;
 
-    for (let index = 0; index < obj.length; index++) {
+    document.getElementById("numeroKetchup").innerHTML = numeroKetchup;
+    document.getElementById("numeroMaionese").innerHTML = numeroMaionese;
+
+    for (let index = 0; index < prodotti.length; index++) {
         /*
             STRUTTURA ESSERE TIPO:
             <div class="spalma">
@@ -23,11 +30,11 @@ function CaricaPagina(){
                 <div class="quantity">6</div>
             </div>
         */
-        if(obj[index] != null){
+        if(prodotti[index] != null){
             //Si prendono i valori di nome, prezzo e quantita per l'oggetto attuale
-            var nomeAttributo = obj[index].nome;
-            var prezzoAttributo = obj[index].prezzo;
-            var quantitaAttributo = obj[index].quantita;
+            var nomeAttributo = prodotti[index].nome;
+            var prezzoAttributo = prodotti[index].prezzo;
+            var quantitaAttributo = prodotti[index].quantita;
 
             //Si creano i DIV
             const divSpalma = document.createElement("div");
@@ -95,7 +102,7 @@ function CaricaPagina(){
                 divRifiuta.innerHTML = "Rifiuta";
 
         //Si inseriscono i DIV nel body
-        var divBottoni = document.querySelector(".lista");
+        var divBottoni = document.querySelector("#bottone");
         divBottoni.appendChild(divSpalma);
         divSpalma.appendChild(divBottoneRosso);
             divBottoneRosso.appendChild(divAccetta);
@@ -116,7 +123,7 @@ function CaricaPagina(){
                 divCompleta.innerHTML = "Completa";
 
         //Si inseriscono i DIV nel body
-        var divBottoni = document.querySelector(".lista");
+        var divBottoni = document.querySelector("#bottone");
         divBottoni.appendChild(divCentra);
             divCentra.appendChild(divCursorPointer);
                 divCursorPointer.appendChild(divCompleta);
@@ -134,5 +141,3 @@ function Rifiuta(){
 function Completa(){
     alert("COMPLETA");
 }
-
-//TODO: Aggiungere le funzioni vere e proprie ai bottoni
