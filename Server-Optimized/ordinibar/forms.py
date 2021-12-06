3db2c8352b41182da9ff9a62e245080d3cbe718f
davidebianchi03 <82215406+davidebianchi03@users.noreply.github.com>
2021-12-06 16:43:08 +0100

    Cambio email

--- Server-Optimized/ordinibar/forms.py
+++ Server-Optimized/ordinibar/forms.py
@@ -4,11 +4,13 @@ from django.forms.widgets import PasswordInput, TextInput
 
 #form per il cambio della password dell'utente
 class ChangePasswordForm(forms.Form):
+    request_type = forms.CharField()
     vecchia_password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Vecchia password',}))
     nuova_password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Nuova password',}))
     conferma_password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Conferma password',}))
 
 class ChangeEmailForm(forms.Form):
-    vecchia_email = forms.EmailField()
-    nuova_password = forms.EmailField()
-    conferma_password = forms.EmailField()
+    request_type = forms.CharField()
+    password =  forms.CharField(widget=PasswordInput(attrs={'placeholder':'Password'}))
+    nuova_email = forms.EmailField()
+    conferma_email = forms.EmailField()
